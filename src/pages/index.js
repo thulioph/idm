e847f56ad26457881de6b3b158e9bf189e494c5e
Thulio Philipe <thulioph@gmail.com>
2020-10-25 01:47:26 -0300

    feat(ui-home): updates home page to fetch artist info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,41 @@
+import fetch from 'node-fetch';
 import React, { useState } from 'react';
 import Head from 'next/head';
 
-import { Button } from 'reactstrap';
+import {
+  Button,
+  Jumbotron,
+  Input,
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function HomePage(props) {
   const [state, setState] = useState({});
+  const [artist, setArtist] = useState('');
 
   // should be an external file handling this request
   const handleBtnClick = async () => {
-    const res = await fetch('http://localhost:3000/api/hello');
+    setState({});
+
+    const res = await fetch('http://localhost:3000/api/artist', {
+      method: 'POST',
+      body: JSON.stringify({ artist }),
+    });
+
     const data = await res.json();
 
     setState(data);
+  };
+
+  const handleInputChange = (evt) => {
+    const { value } = evt.target;
+
+    setArtist(value);
+    setState({});
   };
 
   return (
@@ -24,15 +46,41 @@ function HomePage(props) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      {/* should be a component */}
-      <h1>
-        Dale, {props.name} - {JSON.stringify(state)}
-      </h1>
+      <Jumbotron>
+        <h1 className="display-3">Hello, {props.name}!</h1>
 
-      {/* should be a component */}
-      <Button color="danger" onClick={handleBtnClick}>
-        call hello
-      </Button>
+        <p className="lead">
+          This is a simple hero unit, a simple Jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+
+        <hr className="my-2" />
+
+        <p>Search by your favorite artist:</p>
+
+        <p>
+          <Input
+            value={artist}
+            onChange={handleInputChange}
+            placeholder="artist name"
+          />
+        </p>
+
+        <p className="lead">
+          <Button color="primary" onClick={handleBtnClick}>
+            search
+          </Button>
+        </p>
+      </Jumbotron>
+
+      <Container fluid>
+        <Row>
+          <Col>
+            <code>{JSON.stringify(state)}</code>
+          </Col>
+        </Row>
+      </Container>
+
     </React.Fragment>
   );
 }
@@ -41,12 +89,12 @@ export default HomePage;
 
 // should be an external file handling this request
 export async function getStaticProps() {
-  const res = await fetch('https://api.github.com/users/thulioph');
-  const user = await res.json();
+  // const res = await fetch('https://api.github.com/users/thulioph');
+  // const user = await res.json();
 
   return {
     props: {
-      ...user,
+      name: 'User'
     },
     revalidate: 1,
   };
